c508f6469a29388e0c37141a5a71b5be181db7e3
aliyev <aliyevali04@gmail.com>
2023-07-21 01:57:18 +0400

    modify Navbar

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import CreateCourse from "./pages/create-course";
 import MyCourses from "./pages/my-courses";
+import CourseDetails from "./pages/CourseDetails";
+
+import "./App.css";
+import Categories from "./pages/categories";
 
 function App() {
   const {
@@ -26,94 +30,96 @@ function App() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="true"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" to="/">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="pricing">
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="courses">
-                  All Courses
-                </NavLink>
-              </li>
-              {userRole === "instructor" && (
-                <React.Fragment>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="my-courses">
-                      My Courses
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="create-course">
-                      Create a Course
-                    </NavLink>
-                  </li>
-                </React.Fragment>
-              )}
-              {!isLoggedIn ? (
-                <li className="nav-item dropdown">
-                  <NavLink
-                    className="nav-link dropdown-toggle"
-                    to="login"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Join us
-                  </NavLink>
-                  <ul className="dropdown-menu dropdown-menu-dark">
-                    <li>
-                      <NavLink className="dropdown-item" to="login">
-                        Login
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink className="dropdown-item" to="register">
-                        Register
-                      </NavLink>
-                    </li>
-                  </ul>
-                </li>
-              ) : (
-                <li className="nav-item">
-                  <button className="nav-link" onClick={handleLogout}>
-                    Logout
-                  </button>
-                </li>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+  <NavLink className="navbar-brand logo" to="/"></NavLink>
+  <button
+    className="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarNavDropdown"
+    aria-controls="navbarNavDropdown"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    {/* Use Bootstrap's Navbar Toggler Icon */}
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <NavLink className="nav-link" aria-current="page" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/categories">
+          Categories
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/courses">
+          All Courses
+        </NavLink>
+      </li>
+      {userRole === "instructor" && (
+        <>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/my-courses">
+              My Courses
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/create-course">
+              Create a Course
+            </NavLink>
+          </li>
+        </>
+      )}
+    </ul>
+    <form className="d-flex ms-auto">
+      {window.innerWidth >= 768 ? (
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+      ) : (
+        <span className="navbar-toggler-icon"></span>
+      )}
+      <button className="btn btn-outline-primary" type="submit">
+        Search
+      </button>
+    </form>
+    <div className="navbar-nav ms-auto auth-elements">
+      {!isLoggedIn ? (
+        <>
+          <NavLink className="nav-link" to="/login">
+            Log In
+          </NavLink>
+          <NavLink className="nav-link" to="/register">
+            Sign Up
+          </NavLink>
+        </>
+      ) : (
+        <button className="nav-link" onClick={handleLogout}>
+          Logout
+        </button>
+      )}
+    </div>
+  </div>
+</nav>
+
       <Routes>
         <Route path="/" element={<Test />} />
         <Route path="/courses" element={<Courses />} />
+        <Route path="/categories" element={<Categories />} />
         {userRole === "instructor" && (
-          <Route path="/create-course" element={<CreateCourse />} />
-        )}
+            <Route path="/create-course" element={<CreateCourse />} />
+          ) && <Route path="/my-courses" element={<MyCourses />} />}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/my-courses" element={<MyCourses />} />
+        <Route path="/courses/:courseId" element={<CourseDetails />} />
       </Routes>
     </div>
   );

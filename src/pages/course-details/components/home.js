6c2438625c3d79a90788735b4fe0f812268adf52
aliyev <aliyevali04@gmail.com>
2023-08-06 17:43:01 +0400

    home page design change

--- src/pages/course-details/components/home.js
+++ src/pages/course-details/components/home.js
@@ -1,19 +1,57 @@
 import React from "react";
-import { Card, Col, Row } from "antd";
+import { Card, Col, Row, Collapse } from "antd";
+import DeadlinesList from "./deadlines-list";
+
+const newItems = [
+  {
+    label: (
+      <>
+        <span style={{ color: "#188ac4", fontWeight: "bold" }}>Today </span>
+        <span> (0)</span>
+      </>
+    ),
+    children: <p>{"text"}</p>,
+  },
+];
 
 const HomeComponent = () => {
   return (
     <div>
       <Row gutter={16}>
-        <Col xs={24} sm={24} md={16} style={{ minWidth: 200 }}>
-          <Card title="Card title" bordered={true}>
+        <Col
+          xs={20}
+          sm={20}
+          md={12}
+          style={{ minWidth: 200, marginBottom: "1rem" }}
+        >
+          <Card
+            title="My Announcements"
+            bordered={true}
+            style={{ marginBottom: "2rem" }}
+          >
             This is a card about the first card in the first card card about the
             first card in the first card
           </Card>
+          <Card
+            title="My Tasks"
+            bordered={true}
+            style={{ marginBottom: "2rem" }}
+          >
+            This is a card about the first card in the first card card about the
+            first card in the first card
+          </Card>
+
+          <Card
+            title="What is new"
+            bordered={true}
+            style={{ marginBottom: "2rem" }}
+          >
+            <Collapse ghost style={{ fontWeight: "bold" }} items={newItems} />
+          </Card>
         </Col>
-        <Col xs={24} sm={24} md={8} style={{ minWidth: 150 }}>
-          <Card title="Card title" bordered={true}>
-            Card content
+        <Col xs={20} sm={20} md={12} style={{ minWidth: 200 }}>
+          <Card title="To do" bordered={true}>
+            <DeadlinesList />
           </Card>
         </Col>
       </Row>
